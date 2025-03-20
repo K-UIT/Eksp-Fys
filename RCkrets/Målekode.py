7d3a1d693e0e12b5bc9ea5975b8a0c6676f1da5f
KAL-UIT <kle057@uit.no>
2025-03-20 20:53:11 +0100

    Update Målekode.py

--- RCkrets/Målekode.py
+++ RCkrets/Målekode.py
@@ -4,67 +4,64 @@ import matplotlib.pyplot as plt
 import pyvisa
 import time
 
-# Print available resources connected to the computer
+# Kobler seg til Oscillioskopet
 rm = pyvisa.ResourceManager()
 resources = rm.list_resources()
-
-# Open the oscilloskope
 osc = rm.open_resource(resources[0])
 
-# Default setup of oscilloscope
+# Default setup av oscilloscope
 osc.write(r':SYSTem:PRESet')
 osc.write(r':CHANnel2:DISPlay on')
 osc.write(r':CHANnel2:PROBe 1')
 
-# Start wave generator on oscilloscope: 5V sinus
+# Starter wave generator med 5V sinus
 osc.write(r'WGEN:OUTPut on')
 osc.write(r"WGEN:FUNCtion SINusoid")
 osc.write(r"WGEN:VOLTage 5")
 
-# Create arrays for frequency, voltage at channel 1, voltage at channel 2, and phase
+# lager lister for frekvens, volt ved kanal 1, volt ved kanal 2 og fase
 F = []
 V_inn = []
 V_ut = []
 phase = []
 
-# Create logarithmic range for frequency sweep
+# Lager en logaritmisk frekvens "sweep"
 a1 = 10.**(np.arange(3, 6))
 a2 = np.arange(1, 10, 0.1)
 logrange = np.outer(a1, a2).flatten()[:-89]
 print(logrange)
 print(len(logrange))
 
-# Sweep all frequencies and measure voltage in, voltage out, and phase
+#Går gjennom alle frekvensene
 for i in logrange:
-    osc.write(f':WGEN:FREQuency {i}Hz')  # Set frequency
-    osc.write(':AUTOSCALE channel1,channel2')  # Autoscale channel 1
-    #osc.write(':AUTOSCALE channel2')  # Autoscale channel 2
-    time.sleep(0.5)  # Allow time for stabilization
+    osc.write(f':WGEN:FREQuency {i}Hz')  # Setter frekvens
+    osc.write(':AUTOSCALE channel1,channel2')  # Autoscale channel 1 og 2
+    time.sleep(0.5)  # Venter for å få stabil avlesning
 
-    # Measure voltages
+    # Måler spenning
     V_inn.append(float(osc.query(':MEASure:VPP? CHANnel1')))
     V_ut.append(float(osc.query(':MEASure:VPP? CHANnel2')))
 
-    # Measure phase
+    # Måler fase
     phase.append(float(osc.query(':MEASure:PHASe? CHANnel1,CHANnel2')))
 
-    # Store frequency
+    # Lagrer frekvens
     F.append(i)
 
-# Save frequency and voltages as pandas dataframe
+# Lagrer alt som en dataframe
 df = pd.DataFrame({'F': F, 'V_inn': V_inn, 'V_ut': V_ut, 'phase': phase})
 df = df.replace('E', 'e', regex=True).replace(',', '.', regex=True)
 df = df.apply(pd.to_numeric, errors='coerce')
 
-# Create column with the transfer function in dB
+# Lagrer en kollonne med dB verdiene
 df['dB'] = 20 * np.log10(df["V_ut"] / df["V_inn"])
 
-# Print the pandas dataframe for a visual check
+# Printer ut dataframen
 print(df)
-# Save the pandas dataframe as a CSV file
+# Lagrer alt i en csv fil
 df.to_csv(str(input("Filnavn: ")+".csv"))
 
-# Plot amplitude (in dB) and phase against frequency
+# Plotter for sjekk
 fig, axes = plt.subplots(2, 1)
 
 axes[0].plot(df["F"], df["dB"])
